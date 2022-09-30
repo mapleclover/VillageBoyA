ccbf4e07c65b68d02d6162d23d074f6ac5f51ac8
qkrdudwns000 <114732265+qkrdudwns000@users.noreply.github.com>
2022-09-30 19:57:45 +0900

    YoungJunTest

---
+++ Assets/01.Scripts/YoungJun.cs
@@ -0,0 +1,21 @@
+//박영준
+//풀푸쉬 테스트용
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoungJun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
