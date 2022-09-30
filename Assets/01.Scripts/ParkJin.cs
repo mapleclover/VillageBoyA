3e07fcf0091f64bf1345bb35729fec1347cbdddb
ParkJin0814 <isklkl97@gmail.com>
2022-09-30 19:25:21 +0900

    박진 테스트용
    테스트용

---
+++ Assets/01.Scripts/ParkJin.cs
@@ -0,0 +1,20 @@
+///작성자 : 박진
+///설명 : 테스트용
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkJin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
