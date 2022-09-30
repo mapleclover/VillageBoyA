e1d9e4d9c49b46e9ca6606ef1fb4a7fe14ccb982
mapleclover <mapleclove@gmail.com>
2022-09-30 18:32:42 +0900

    StartUpCommit

---
+++ Assets/01.Scripts/CharacterInfomation.cs
@@ -0,0 +1,21 @@
+///작성자 : 유은호
+///설명 : 캐릭터의 정보를 담기 위한 클래스
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfomation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
