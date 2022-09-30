23b973b41ab758f70215212c1402c4bb983f1280
Yeongjin1017 <114732304+Yeongjin1017@users.noreply.github.com>
2022-09-30 20:17:37 +0900

    testing

---
+++ Assets/01.Scripts/ParkJin2.cs
@@ -0,0 +1,20 @@
+///작성자 : 박진
+///설명 : 테스트용2
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkJin2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
