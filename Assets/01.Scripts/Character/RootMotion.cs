5f3c487e5af8b961efb7bb5e6bb55ff1611f15e0
jjw0487 <jjw0487@gmail.com>
2022-10-13 21:47:36 +0900

    RootMotion

---
+++ Assets/01.Scripts/Character/RootMotion.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RootMotion : MonoBehaviour
+{
+    public bool DontMove = false;
+    Vector3 deltaPosition = Vector3.zero;
+    Quaternion deltaRotation = Quaternion.identity;
+
+
+    private void FixedUpdate()
+    {
+        if (DontMove) return;
+        transform.parent.Translate(deltaPosition, Space.World);
+        deltaPosition = Vector3.zero;
+        transform.parent.rotation *= deltaRotation;
+        deltaRotation = Quaternion.identity;
+    }
+    private void OnAnimatorMove() // 
+    {
+        deltaPosition += GetComponent<Animator>().deltaPosition;
+        deltaRotation *= GetComponent<Animator>().deltaRotation;
+
+    }
+}
