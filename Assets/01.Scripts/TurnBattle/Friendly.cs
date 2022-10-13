99940f46283315af43acb40a95ca5de5ee026b24
ParkJin0814 <isklkl97@gmail.com>
2022-10-13 21:45:07 +0900

    Jin10.13
    캐릭터가 타겟을향해 방향변경 공격이후 제자리로 돌아와서 원래방향 응시

--- Assets/01.Scripts/TurnBattle/Friendly.cs
+++ Assets/01.Scripts/TurnBattle/Friendly.cs
@@ -8,6 +8,8 @@ public class Friendly : CharacterProperty
     public float myHp = 100.0f;
     public float speed = 2.0f;
     public int Skill = 0;
+    public GameObject Target;
+    public bool Active5=true;
     void Start()
     {
         
@@ -32,5 +34,23 @@ public class Friendly : CharacterProperty
                 break;
         }
     }
-    
+    public void RandomSkill()
+    {
+        int rnd = Random.Range(0, 3);
+        switch (rnd)
+        {
+            case 0:
+                myAnim.SetTrigger("Attack");
+                break;
+            case 1:
+                myAnim.SetTrigger("Attack2");
+                break;
+            case 2:
+                myAnim.SetTrigger("Attack3");
+                break;
+            default:
+                break;
+        }
+    }
+
 }
