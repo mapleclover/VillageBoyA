d720ed762913ffaeee4a5c622db488fa5cdcc659
jjw0487 <jjw0487@gmail.com>
2022-10-17 23:38:46 +0900

    OngoingStaminaBar

---
+++ Assets/01.Scripts/Movement/Stamina/StaminaBar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    //public Transform myTarget;
+    //public Slider myBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        /*Vector3 pos = Camera.main.WorldToScreenPoint(myTarget.position);    // 3차원 공간안의 월드 좌표값을 좌표를 2D 화면상의 위치으로 바꿔준다.
+        if (pos.z < 0.0f)
+        {
+            transform.position = new Vector3(0, 10000, 0);
+
+        }
+        else
+        {
+            transform.position = pos;
+        }
+        transform.position = pos;*/
+    }
+}
