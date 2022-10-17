265255f0ab2807f67137bb80321c793961106822
ParkJin0814 <isklkl97@gmail.com>
2022-10-17 22:03:50 +0900

    Jin10.17
    1.스피드체크하여 순서정하기
    2.순서대로 공격체크 마지막공격확인

--- Assets/01.Scripts/TurnBattle/BattleCharacter.cs
+++ Assets/01.Scripts/TurnBattle/BattleCharacter.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Friendly : CharacterProperty
+public class BattleCharacter : CharacterProperty
 {
     public float myHp = 100.0f;
     public float speed = 2.0f;
@@ -24,14 +24,17 @@ public class Friendly : CharacterProperty
     {
         switch(s)
         {
-            case 1:
+            case 0:
                 myAnim.SetTrigger("Attack");
+                myTarget.GetComponent<BattleCharacter>().OnDamage(10.0f);
+                break;
+            case 1:
+                myAnim.SetTrigger("Attack2");
+                myTarget.GetComponent<BattleCharacter>().OnDamage(20.0f);
                 break;
             case 2:
-                myAnim.SetTrigger("Attack2");
-                break;
-            case 3:
                 myAnim.SetTrigger("Attack3");
+                myTarget.GetComponent<BattleCharacter>().OnDamage(30.0f);
                 break;
         }
     }
@@ -42,16 +45,34 @@ public class Friendly : CharacterProperty
         {
             case 0:
                 myAnim.SetTrigger("Attack");
+                myTarget.GetComponent<BattleCharacter>().OnDamage(10.0f);
                 break;
             case 1:
                 myAnim.SetTrigger("Attack2");
+                myTarget.GetComponent<BattleCharacter>().OnDamage(20.0f);
                 break;
             case 2:
                 myAnim.SetTrigger("Attack3");
+                myTarget.GetComponent<BattleCharacter>().OnDamage(30.0f);
                 break;
             default:
                 break;
         }
+    }
+    public void OnDamage(float dmg)
+    {
+        switch(Random.Range(0,100))
+        {
+            case 0:
+                dmg *= 2;                
+                break;
+            case 1:
+                dmg *= 0;
+                break;
+            default:                
+                break;
+        }
+        myHp -= dmg;
     }
 
 }
