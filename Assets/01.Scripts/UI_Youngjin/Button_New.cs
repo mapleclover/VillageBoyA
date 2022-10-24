df2f5eeaf1e2d0962a30494e884bfc51df797090
Yeongjin1017 <114732304+Yeongjin1017@users.noreply.github.com>
2022-10-24 13:34:39 +0900

    1024_LYJ

--- Assets/01.Scripts/UI_Youngjin/Button_New.cs
+++ Assets/01.Scripts/UI_Youngjin/Button_New.cs
@@ -11,19 +11,20 @@ public class Button_New : MonoBehaviour
     public static int cur = 0;
     public GameObject OnMouse;
     public GameObject[] myButtons;
+    public GameObject[] myButtons2;
     public GameObject mySaveLoad;
     public GameObject myStart;
-    public GameObject myCreate;
+
 
     bool check = false;
     bool v = false;
-    public int min = 0, max = 1;
+    int min = 0, max = 3;
     Vector2 pos = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        max = myButtons.Length - 1;
+
     }
 
     // Update is called once per frame
@@ -32,57 +33,59 @@ public class Button_New : MonoBehaviour
 
         if (v)
         {
-            Move();
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (mySaveLoad.activeSelf)
             {
-                switch (cur)
+                SecondMove();
+                OnMouse.transform.localScale = new Vector3(1.6f, 0.9975f, 0.9975f);
+                OnMouse.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(30, 50);
+                OnMouse.transform.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(30, 50);
+                if (Input.GetKeyDown(KeyCode.Escape))
                 {
-                    case 0:
-                        OnClickStart();
-                        break;
-                    case 1:
-                        OnClickSettings();
-                        break;
-                    case 2:
-                        OnClickExit();
-                        break;
+                    if (check)
+                    {
+                        OnClickBack();
+                        check = false;
+                    }
+                }
+
+            }
+            else
+            {
+                OnMouse.transform.localScale = new Vector3(0.9975f, 0.9975f, 0.9975f);
+                OnMouse.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(50, 50);
+                OnMouse.transform.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(50, 50);
+                Move();
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    switch (cur)
+                    {
+                        case 0:
+                            Select.instance.Game();
+                            break;
+                        case 1:
+                            OnClickStart();
+                            break;
+                        case 2:
+                            OnClickSettings();
+                            break;
+                        case 3:
+                            OnClickExit();
+                            break;
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    cur++;
+                    if (cur > max) cur = max;
+                }
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    cur--;
+                    if (cur < min) cur = min;
                 }
             }
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                cur++;
-                if (cur > max) cur = max;
-            }
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                cur--;
-                if (cur < min) cur = min;
-            }
 
-        }
-        if (mySaveLoad.activeSelf)
-        {
-            OnMouse.transform.localScale = new Vector3(1.6f, 0.9975f, 0.9975f);
-            OnMouse.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(30, 50);
-            OnMouse.transform.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(30, 50);
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                if (check && !myCreate.activeSelf)
-                {
-                    OnClickBack();
-                    check = false;
-                }
-            }
 
-        }
-        else
-        {
-            OnMouse.transform.localScale = new Vector3(1.1f, 0.9975f, 0.9975f);
-            OnMouse.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(50, 50);
-            OnMouse.transform.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(50, 50);
-            //x scale:0.9975
-            //width 50
-            //-225
         }
         if (Input.anyKey)
         {
@@ -117,6 +120,11 @@ public class Button_New : MonoBehaviour
     public void Move()
     {
         pos.y = myButtons[cur].transform.localPosition.y;
+        OnMouse.transform.localPosition = pos;
+    }
+    public void SecondMove()
+    {
+        pos.y = myButtons2[cur].transform.localPosition.y;
         OnMouse.transform.localPosition = pos;
     }
 }
