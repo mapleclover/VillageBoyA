ea9c653ae0ac3b5f4cccfdcc3affeb25500270aa
mapleclover <mapleclove@gmail.com>
2022-11-07 19:55:52 +0900

    scenload

--- Assets/01.Scripts/MainScreen/SceneLoad.cs
+++ Assets/01.Scripts/MainScreen/SceneLoad.cs
@@ -34,12 +34,27 @@ public class SceneLoad : MonoBehaviour
     }
 
     bool isChange = false;
+    public bool BackAttack = false;
+    public string[] MonsterType;
+    public int[] MonsterCount;
 
     public void ChangeScene(int i)
     {
         if (!isChange)
         {
             StartCoroutine(Loading(i));
+            
+        }
+    }
+    
+    public void ToBattleScene(bool backAttack, string[] monsterType, int[] monsterCount)
+    {
+        BackAttack = backAttack;
+        MonsterType = monsterType;
+        MonsterCount = monsterCount;
+        if (!isChange)
+        {
+            StartCoroutine(Loading("05.Battle"));
         }
     }
 
@@ -53,10 +68,37 @@ public class SceneLoad : MonoBehaviour
         StartCoroutine(LoadingTarget(slider, i));
         isChange = false;
     }
+    IEnumerator Loading(string mapName)
+    {
+        isChange = true;
+        yield return SceneManager.LoadSceneAsync(0);
+        GameObject obj = GameObject.Find("LoadingGage");
+        Slider slider = obj.GetComponent<Slider>();
+        slider.value = 0.0f;
+        StartCoroutine(LoadingTarget(slider, mapName));
+        isChange = false;
+    }
 
     IEnumerator LoadingTarget(Slider slider,int i)
     {
         AsyncOperation ao = SceneManager.LoadSceneAsync(i);
+        ao.allowSceneActivation = false;
+        while (!ao.isDone)
+        {
+            slider.value = ao.progress / 0.9f;
+            if (Mathf.Approximately(slider.value, 1.0f))
+            {
+                yield return new WaitForSeconds(1.0f);
+                // ¾À·Îµù ³¡
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    IEnumerator LoadingTarget(Slider slider, string mapName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(mapName);
         ao.allowSceneActivation = false;
         while (!ao.isDone)
         {
