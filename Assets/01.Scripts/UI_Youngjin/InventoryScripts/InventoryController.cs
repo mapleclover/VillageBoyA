39f155650fa81bbc0dd2b43b9d3f39b2e440c064
Yeongjin1017 <114732304+Yeongjin1017@users.noreply.github.com>
2022-11-16 01:28:54 +0900

    1116_LYJ

--- Assets/01.Scripts/UI_Youngjin/InventoryScripts/InventoryController.cs
+++ Assets/01.Scripts/UI_Youngjin/InventoryScripts/InventoryController.cs
@@ -22,6 +22,7 @@ public class InventoryController : MonoBehaviour
     public int defaultCount = 1;
     public List<GameObject> curItem=new List<GameObject>();
     bool v = true;
+    Vector3 currentRotation;
 
     private void Awake()
     {
