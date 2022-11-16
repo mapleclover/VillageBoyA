d7eb1abf3f5787962b359ea236ecf2494bba2c03
jjw0487 <jjw0487@gmail.com>
2022-11-16 19:47:45 +0900

    AboutItem.cs

--- Assets/01.Scripts/YoungJunScriot/Item.cs
+++ Assets/01.Scripts/YoungJunScriot/Item.cs
@@ -27,6 +27,7 @@ public class Item : ScriptableObject
 
     // 강화 ---------------------------------
 
+    [Header("------Enhancement-------")]
     // 외부수정불가
     [SerializeField] EnhanceableItem _enhanceableItem;
     public EnhanceableItem enhanceableItem // 강화 가능 한 아이템인지
@@ -43,8 +44,6 @@ public class Item : ScriptableObject
     public int[] AP; // 공격력
     public int[] EnchantCost; // 강화비용
     public ItemLevel itemLevel; // 아이템 레벨 5까지 설정
-     
-
     public bool CheckSuccess(int level)
     {
         float rnd = Random.Range(0f, 100f);
@@ -57,6 +56,8 @@ public class Item : ScriptableObject
 
     // 강화 ----------------------------------
 
+
+    
     public enum NpcType
     {
         Weapon,
