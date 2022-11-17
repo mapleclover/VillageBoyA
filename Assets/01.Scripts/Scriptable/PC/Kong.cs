b8b67bc700423305580d036b175c69ad34cd529f
mapleclover <mapleclove@gmail.com>
2022-11-18 01:07:58 +0900

    ScriptableObject merge

---
+++ Assets/01.Scripts/Scriptable/PC/Kong.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new pc", menuName = "ScriptableObject/PC")]
+public class PC : ScriptableObject
+{
+    
+}
--- Assets/01.Scripts/YoungJunScriot/EnemySC.cs
+++ Assets/01.Scripts/YoungJunScriot/EnemySC.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "new enemy", menuName = "new enemy/enemy")]
+[CreateAssetMenu(fileName = "new enemy", menuName = "ScriptableObject/Enemy")]
 public class EnemySC : ScriptableObject
 {
     public string enemyName;
