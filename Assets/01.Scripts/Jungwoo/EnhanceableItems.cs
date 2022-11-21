5d7b99b62229a59a7c787a00061095bcb6ebf38d
jjw0487 <jjw0487@gmail.com>
2022-11-21 21:51:17 +0900

    See ya soon

--- Assets/01.Scripts/Jungwoo/EnhanceableItems.cs
+++ Assets/01.Scripts/Jungwoo/EnhanceableItems.cs
@@ -1,20 +1,8 @@
 //작성자 : 전정우
 //설명 :
 using System;
+using UnityEditor;
 using UnityEngine;
-
-[Serializable]
-public struct SaveItemData
-{
-    public Item orgData;
-
-    public int Itemlevel; // �ν����Ϳ� ���� �� �ְ� �����
-
-    public float GetPossibility(int level)
-    {
-        return orgData.possibility[(int)level];
-    }
-}
 
 public class EnhanceableItems : MonoBehaviour
 {
