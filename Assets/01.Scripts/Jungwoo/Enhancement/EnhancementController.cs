a60725a8eafb79efce148488a7a85d30cb10df19
jjw0487 <jjw0487@gmail.com>
2022-11-23 19:03:05 +0900

    for pulling

--- Assets/01.Scripts/Jungwoo/Enhancement/EnhancementController.cs
+++ Assets/01.Scripts/Jungwoo/Enhancement/EnhancementController.cs
@@ -36,11 +36,13 @@ public struct SaveItemData
     // 2. orgData를 인벤토리에서 끌어왔을 때 자동으로 바인딩 되도록 바꿔야 함.
     // 완료 3. Level 배열 최댓값이 아직 잡히지 않음 (집에가서 해)
     // 4. 강화 후 데이터가 출력되는 툴팁 필요 그리고 캐릭터 스텟에 적용@@@@@@
-    // 5. 스크립터블에서 필요 재료 혹은 재화를 연동 해야함.
+    // 5. 스크립터블에서 필요 재료 혹은 재화를 연동 해야함. >> 슬롯에 자동추가 >> 내 아이템 리스트에서
     // 완료 6. 강화 버튼 함수를 따로 만들어야 함
     // 완료 7. 강화 가능 유무 로직을 만들어야 함 (아에 강화 슬롯에 올라가지 않거나 올라가면 버튼 UI가 false)
 
     // 재료강화 >> 텍스트로
+
+
 
     // 일단 여기에 스트럭트 구조를 만들었지만 다 모였을 때 EnhanceableItems.cs 를
     // 스크립터블로 만들고 거기에서 아이템데이터 구조를 만들 수 있도록 해야할 것 같음
@@ -61,7 +63,7 @@ public class EnhancementController : MonoBehaviour
     public GameObject mySlot;
     public Item myItems;
 
-    public GameObject[] myMaterial = new GameObject[2];
+    public GameObject myMaterial = null;
 
 
 
@@ -103,8 +105,11 @@ public class EnhancementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (myMaterial[0].transform.childCount > 0 && myMaterial[1].transform.childCount > 0)
+        {
 
-        myMaterial[0] = transform.GetChild(0).gameObject;
+        }
+        myMaterisdfww = myMaterial[0].transform.GetChild(0).gameObject;
         //GetComponent<Pickup>().item.value;
         myMaterial[1] = transform.GetChild(0).gameObject;
 
